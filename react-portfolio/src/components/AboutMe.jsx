217a48eddee732ee5f511ad4513a272aa1808b22
pattyboyy <134738449+pattyboyy@users.noreply.github.com>
2024-07-25 18:20:30 -0600

    fixed image

--- react-portfolio/src/components/AboutMe.jsx
+++ react-portfolio/src/components/AboutMe.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import profileImage from '../assets/images/IMG_2208.jpeg';
 
 function AboutMe() {
   return (
     <section className="about-me">
-      <img src="react-portfolio/src/assets/images/IMG_2208.jpeg" alt="Patrick Riedinger" />
+      <img src={profileImage} alt="Your Name" />
       <div>
         <h2>About Me</h2>
         <p>
-          Hello! I'm Patrick Riedinger, a passionate web developer with expertise in 
+          Hello! I'm [Your Name], a passionate web developer with expertise in 
           creating responsive and user-friendly web applications. With a strong 
           foundation in HTML, CSS, and JavaScript, and experience with modern 
           frameworks like React, I strive to build efficient and scalable 
